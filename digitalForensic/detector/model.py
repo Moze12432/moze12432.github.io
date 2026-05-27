8996063d67e53d7dab1937d56dcf99104e668c18
MUKIIBI MOSES <mosesmukiibi21@gmail.com>
2026-05-28 03:00:28 +0900

    Fix indentation and duplicate lines in load_detector
    Signed-off-by: MUKIIBI MOSES <mosesmukiibi21@gmail.com>

--- digitalForensic/detector/model.py
+++ digitalForensic/detector/model.py
@@ -5,7 +5,17 @@ def load_detector():
     interpreter = tflite.Interpreter(
         model_path="models/deepfake_detector.tflite"
     )
+import tflite_runtime.interpreter as tflite
 
+def load_detector():
+
+    interpreter = tflite.Interpreter(
+        model_path="models/deepfake_detector.tflite"
+    )
+
+    interpreter.allocate_tensors()
+
+    return interpreter
     interpreter.allocate_tensors()
 
     return interpreter
