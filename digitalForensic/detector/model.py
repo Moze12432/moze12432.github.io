1e01df864b59196a69f4053cd7783683db889a77
MUKIIBI MOSES <mosesmukiibi21@gmail.com>
2026-05-28 03:00:48 +0900

    Update model.py
    Signed-off-by: MUKIIBI MOSES <mosesmukiibi21@gmail.com>

--- digitalForensic/detector/model.py
+++ digitalForensic/detector/model.py
@@ -5,17 +5,7 @@ def load_detector():
     interpreter = tflite.Interpreter(
         model_path="models/deepfake_detector.tflite"
     )
-import tflite_runtime.interpreter as tflite
 
-def load_detector():
-
-    interpreter = tflite.Interpreter(
-        model_path="models/deepfake_detector.tflite"
-    )
-
-    interpreter.allocate_tensors()
-
-    return interpreter
     interpreter.allocate_tensors()
 
     return interpreter
