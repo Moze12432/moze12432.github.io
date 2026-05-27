d968ad7aaf0de16f7bcb931ece72b182a7c5e0bd
MUKIIBI MOSES <mosesmukiibi21@gmail.com>
2026-05-28 03:38:04 +0900

    Add debug information for model inputs
    Added debug print statements to display model input details.
    
    Signed-off-by: MUKIIBI MOSES <mosesmukiibi21@gmail.com>

--- digitalForensic/detector/model.py
+++ digitalForensic/detector/model.py
@@ -6,5 +6,9 @@ MODEL_PATH = os.path.join(BASE_DIR, "models", "deepfake_detector.onnx")
 
 session = ort.InferenceSession(MODEL_PATH)
 
+# Debug: print model input details
+for inp in session.get_inputs():
+    print(f"Input name: {inp.name}, shape: {inp.shape}, type: {inp.type}")
+
 def load_model():
     return session
