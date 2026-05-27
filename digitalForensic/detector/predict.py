64a940c4b45cbb6baa44fcb2ce2fa9f7d9ef7aa2
MUKIIBI MOSES <mosesmukiibi21@gmail.com>
2026-05-28 03:39:59 +0900

    Refactor image preprocessing to use dynamic dimensions
    Updated image preprocessing to use model's input shape.
    
    Signed-off-by: MUKIIBI MOSES <mosesmukiibi21@gmail.com>

--- digitalForensic/detector/predict.py
+++ digitalForensic/detector/predict.py
@@ -1,36 +1,29 @@
 import numpy as np
 from PIL import Image
-
 from detector.model import load_model
 
 session = load_model()
 
 def preprocess_image(image_path):
+    # Get expected input shape from model
+    input_info = session.get_inputs()[0]
+    _, height, width, channels = input_info.shape
 
     image = Image.open(image_path).convert("RGB")
-
-    image = image.resize((224, 224))
+    image = image.resize((width, height))
 
     img = np.array(image).astype(np.float32)
-
     img = img / 255.0
-
     img = np.expand_dims(img, axis=0)
 
     return img
 
 
 def predict_image(image_path):
-
     img = preprocess_image(image_path)
 
     input_name = session.get_inputs()[0].name
-
-    outputs = session.run(
-        None,
-        {input_name: img}
-    )
-
+    outputs = session.run(None, {input_name: img})
     prediction = outputs[0][0][0]
 
     return float(prediction)
